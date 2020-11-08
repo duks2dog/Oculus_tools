8ad47ca124df6e7a8c8891f977ea4874349ebe93
duks2dog <duks2dog@kabokabo.page>
2020-11-08 14:46:18 +0900

    Create WebCamDrawer.cs

---
+++ WebCamDrawer.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+
+public class WebCamDrawer : MonoBehaviour
+{
+    public string deviceNameKeyword = "THETA";
+
+    void Start()
+    {
+        StartStreaming();
+    }
+
+    void StartStreaming()
+    {
+        WebCamDevice device = new WebCamDevice();
+        // 参照渡しでdeviceを書き換え
+        if (!FindDevice(ref device))
+        {
+            Debug.LogError("<" + deviceNameKeyword + ">を含むWebカメラが検出できませんでした。");
+            return;
+        }
+        WebCamTexture webcamTexture = new WebCamTexture(device.name);
+        Material mat = GetTargetMaterial();
+        if (mat == null)
+        {
+            return;
+        }
+        mat.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    bool FindDevice(ref WebCamDevice target)
+    {
+        bool deviceExists = false;
+        WebCamDevice[] devices = WebCamTexture.devices;
+        foreach (WebCamDevice device in devices)
+        {
+            Debug.Log("device.name => " + device.name);
+            if (device.name.Contains(deviceNameKeyword))
+            {
+                target = device;
+                deviceExists = true;
+            }
+        }
+        return deviceExists;
+    }
+
+    Material GetTargetMaterial()
+    {
+        Skybox skybox = GetComponent<Skybox>();
+        if (skybox != null)
+        {
+            return skybox.material;
+        }
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            return renderer.material;
+        }
+        Debug.LogError("Renderer/Skyboxコンポーネントがありません。");
+        return null;
+    }
+}
