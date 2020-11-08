ac6fcea07b5728ab8e6100d20bf2663c61d844b8
duks2dog <duks2dog@kabokabo.page>
2020-11-08 16:58:26 +0900

    Update WebCamController.cs

--- WebCamController.cs
+++ WebCamController.cs
@@ -5,54 +5,49 @@ namespace OpenCvSharp
     using UnityEngine;
     using UnityEngine.UI;
 
-    public class WebCamController : MonoBehaviour
-    {
+    public class WebCamController : MonoBehaviour {
 
-        int width = 1920;
-        int height = 1080;
-        int fps = 30;
         Texture2D cap_tex;
         Texture2D out_tex;
         WebCamTexture webcamTex;
+        int webcamTexwh = webcamTex.width * webcamTex.height;
+        int width = webcamTex.width;
+        int height = webcamTex.height;
+        int width = 1920;
+        int height = 1080;
+        int fps = 30;
         Color32[] colors = null;
-
-        IEnumerator Init()
-        {
-            while (true)
-            {
-                if (webcamTex.width > 16 && webcamTex.height > 16)
-                {
-                    colors = new Color32[webcamTex.width * webcamTex.height];
+        // WebカメラをInputとして扱う準備
+        // 画像の分のメモリを確保
+        IEnumerator Init() {
+            while (true) {
+                if (webcamTex.width > 16 && webcamTex.height > 16) {
+                    colors = new Color32[webcamTexwh];
                     cap_tex = new Texture2D(webcamTex.width, webcamTex.height, TextureFormat.RGBA32, false);
-                    //GetComponent<Renderer>().material.mainTexture = texture;
                     break;
                 }
                 yield return null;
             }
         }
-        void Start()
-        {
+        // テクスチャとして利用できるようにする
+        void Start() {
+            // WebcamをInputとして受け取る
             WebCamDevice[] devices = WebCamTexture.devices;
             webcamTex = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
             webcamTex.Play();
 
             StartCoroutine(Init());
         }
-        void Update()
-        {
-            if (colors != null)
-            {
+        
+        void Update() {
+            // 全ピクセルを走査しする2重ループ
+            if (colors != null) {
                 webcamTex.GetPixels32(colors);
-
-                int width = webcamTex.width;
-                int height = webcamTex.height;
                 Color32 rc = new Color32(0, 0, 0, byte.MaxValue);
-
-                for (int x = 0; x < width; x++)
-                {
-                    for (int y = 0; y < height; y++)
-                    {
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
                         Color32 c = colors[x + y * width];
+                        // 1ピクセルごとにグレースケール化
                         byte gray = (byte)(0.1f * c.r + 0.7f * c.g + 0.2f * c.b);
                         rc.r = rc.g = rc.b = gray;
                         colors[x + y * width] = rc;
@@ -66,26 +61,18 @@ namespace OpenCvSharp
             GetComponent<RawImage>().texture = out_tex;
         }
 
-        Texture2D To_Mono(Texture2D tex)
-        {
-            //matの定義
+        Texture2D To_Mono(Texture2D tex) {
             Mat mat;
-
             //textureをmatに変換
             mat = Unity.TextureToMat(tex);
-
             //画像をグレスケに変換
-            Mat matGray = mat.CvtColor(ColorConversionCodes.BGR2GRAY);
-
-            //画像を2値化
-            Mat matMono = matGray.Threshold(100, 255, ThresholdTypes.Otsu);
-
-            //2値化画像を白黒反転
-            Cv2.BitwiseNot(matMono, matMono);
-
-            //matMonoをtexture2Dに変換
-            tex = Unity.MatToTexture(matMono);
-
+            Mat gray = mat.CvtColor(ColorConversionCodes.BGR2GRAY);
+            //閾値を指定して、超えたら白となるように二値化
+            //INPUTはグレスケ画像である必要がある
+            Mat mono = gray.Threshold(127, 255, ThresholdTypes.Otsu);
+            //二値化を白黒反転
+            Cv2.BitwiseNot(mono, mono);
+            tex = Unity.MatToTexture(mono);
             return tex;
         }
     }
